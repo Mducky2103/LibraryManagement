19b207f4390f566c4f5bb88029e133fe9fbbd2e5
Ng Duc Minh <ducminh211103@gmail.com>
2025-03-31 00:09:17 +0700

    Fix UI Forgot PW

--- LibraryClient/src/app/user/forgot-password/forgot-password.component.ts
+++ LibraryClient/src/app/user/forgot-password/forgot-password.component.ts
@@ -7,13 +7,15 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './forgot-password.component.html',
   styles: ``
 })
 export class ForgotPasswordComponent implements OnInit {
   form: any;
   isSumitted: boolean = false;
+  attemptCount: number = 0; // Đếm số lần gửi
+  isCooldown: boolean = false; // Kiểm soát cooldown
   constructor(
     public formBuilder: FormBuilder,
     private service: AuthService,
@@ -40,14 +42,37 @@ export class ForgotPasswordComponent implements OnInit {
   submitRequest() {
     if (this.form.invalid) return;
 
+    // Kiểm tra nếu đang trong cooldown
+    if (this.isCooldown) {
+      this.toastr.warning('Bạn đã yêu cầu, vui lòng thử lại sau 1 phút.', 'Chờ giây lát');
+      return;
+    }
+
+    // Kiểm tra nếu đã đạt giới hạn gửi
+    if (this.attemptCount >= 4) {
+      this.toastr.error('Bạn đã vượt quá số lần yêu cầu. Hãy thử lại sau 24 giờ.', 'Quá giới hạn');
+      return;
+    }
     const email = this.form.value.email;
     console.log('Payload:', { email });
+    this.isCooldown = true; // Bắt đầu cooldown
+    this.attemptCount++; // Tăng số lần gửi
     this.service.forgotPassword(email).subscribe({
       next: (response) => {
         this.toastr.success('Password reset link sent to your email.', 'Success');
+
+        if (this.attemptCount >= 4) {
+          setTimeout(() => {
+            this.attemptCount = 0;
+          }, 24 * 60 * 60 * 1000); // 24 giờ
+        }
+        setTimeout(() => {
+          this.isCooldown = false;
+        }, 60000);
       },
       error: (err) => {
         this.toastr.error(err.error?.message || 'An error occurred. Please try again.', 'Error');
+        this.isCooldown = false;
       }
     });
   }
